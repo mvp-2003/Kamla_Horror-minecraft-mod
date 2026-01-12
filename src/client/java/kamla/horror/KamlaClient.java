133e55bc5add477dc8b577654420514ba81dceaf
Rishabh Das <rishabh.babi@gmail.com>
2026-01-12 21:08:41 +0530

    Initial commit

---
+++ src/client/java/kamla/horror/KamlaClient.java
@@ -0,0 +1,10 @@
+package kamla.horror;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class KamlaClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/kamla/horror/KamlaDataGenerator.java
@@ -0,0 +1,11 @@
+package kamla.horror;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class KamlaDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
